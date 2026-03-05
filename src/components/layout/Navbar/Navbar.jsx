0e4db57144b5ae53d359ae4f0418e86bbaf66b4a
Resya-Hasan <resyahasan0@gmail.com>
2026-03-05 15:18:49 +0700

    navbar responsive done

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,38 +1,81 @@
 import Button from '../../ui/Button/Button';
 import styles from './Navbar.module.css';
 import Input from '../../ui/input/Input';
-import { Search } from 'lucide-react'
+import { Search, List } from 'lucide-react'
+import { Link, useNavigate } from 'react-router';
+import { useState, useEffect } from 'react';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const isLogin = !!localStorage.getItem('token');
+    const navigate = useNavigate();
+
+    const handleMovePage = (path) => {
+        navigate(path);
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/login')
+    }
+
     return (
-        <nav className={styles.navabrContainer}>
-            <a href="/" className={styles['navbar-logo']}>Coffee Shop</a>
-            <ul className={styles['navbar-menu']}>
-                <li><a href="/dashboard">Category</a></li>
-                <li><a href="/register">Cart</a></li>
-                <li><a href="/login">Likes</a></li>
-            </ul>
-            <div className={styles['navbar-subContainer']}>
-                <Input
-                    type="text"
-                    placeholder="Search..."
-                    icon={<Search />}
-                    size={"medium"}
-                />
-                <Button
-                    variant={"border-only"}
-                    color="btn-primary"
-                    size={"medium"}
-                    onClick={() => alert('Login clicked!')}
-                >
-                    Login
+        <nav className={styles.navbarContainer}>
+            <Link to="/" className={styles['navbar-logo']}>
+                <h1>MyShop</h1>
+            </Link>
+
+            <Input
+                type="text"
+                placeholder="Search..."
+                icon={<Search />}
+                size={"medium"}
+            />
+
+            <div className={styles.humburger}>
+                <Button variant={"no-bg"} onClick={() => setOpen(!open)}>
+                    <List size={28} />
                 </Button>
-                <Button
-                    size={"medium"}
-                    onClick={() => alert('Register clicked!')}
-                >
-                    Register
-                </Button>
+            </div>
+
+            <div className={`${styles['navbar-subContainer']} ${open ? styles.show : ""}`}>
+                {isLogin && (
+                    <ul className={`${styles['navbar-menu']}`}>
+                        <li><Link to="/category" className={styles.link}>Category</Link></li>
+                        <li><Link to="/cart" className={styles.link}>Cart</Link></li>
+                        <li><Link to="/likes" className={styles.link}>Likes</Link></li>
+                    </ul>
+                )}
+
+
+                {isLogin ? (
+                    <Button
+                        variant={"deafault"}
+                        color="btn-primary"
+                        size={"medium"}
+                        onClick={() => handleLogout()}
+                    >
+                        Logout
+                    </Button>
+                ) : (
+                    <>
+                        <Button
+                            variant={"border-only"}
+                            color="btn-primary"
+                            size={"medium"}
+                            onClick={() => handleMovePage('/login')}
+                        >
+                            Login
+                        </Button>
+                        <Button
+                            size={"medium"}
+                            onClick={() => handleMovePage('/register')}
+                        >
+                            Register
+                        </Button>
+                    </>
+                )}
             </div>
         </nav>
     )
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,10 +4,12 @@ import LoginPage from '../features/auth/LoginPage';
 import Dashboard from '../features/dashboard/Dashboard';
 import ProtectedRoute from '../layouts/protectedRoute';
 import PublicRoute from '../layouts/publicRoute';
+import HomePage from '../features/home/HomePage';
 
 export default function AppRoutes() {
     return (
         <Routes>
+            <Route path='/' element={<HomePage />} />
             <Route element={<PublicRoute />}>
                 <Route path='/register' element={<RegisterPage />} />
                 <Route path='/login' element={<LoginPage />} />
